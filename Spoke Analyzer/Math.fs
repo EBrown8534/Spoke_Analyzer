d7eb96151906de8828939701438be6f86407d89d
EBrown8534 <ebrown8534@gmail.com>
2019-02-12 15:55:47 -0500

    Another fun calculation change, make everything `inline` and SRTP-like, but do it just because we can. No more `rec`.

--- Spoke Analyzer/Math.fs
+++ Spoke Analyzer/Math.fs
@@ -4,14 +4,15 @@ let inline degToRad deg = deg * System.Math.PI / 180.
 let inline radToDeg rad = rad * 180. / System.Math.PI
 let inline angleMath distance angle fn = (distance * (angle |> degToRad |> fn)) |> int 
 
-let getXY distance angle = cos |> angleMath distance angle, sin |> angleMath distance angle
+let inline getXY distance angle = cos |> angleMath distance angle, sin |> angleMath distance angle
 
-let rec getOverlaps smallCount largeCount =
-    if smallCount > largeCount then getOverlaps largeCount smallCount
-    else
+let inline getOverlaps count1 count2 =
+    let inline calculation smallCount largeCount =
         let totalOverlaps = (smallCount |> float) * (largeCount |> float)
         // If the small divides into large evenly, then there are `small` coincident spokes, and we'll divide our total
         // overlap count by that number.
         if largeCount % smallCount <> 0 then totalOverlaps
         else totalOverlaps / (smallCount |> float)
-        
+
+    if count1 > count2 then calculation count2 count1
+    else calculation count1 count2 
